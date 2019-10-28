9a16c0b2affab2083090c949491120e8100613d4
Jon <xuchaoo@gmail.com>
2019-10-28 15:54:34 +0800

    测试最长递增长度

---
+++ test/test_max_successive_series_len.py
@@ -0,0 +1,14 @@
+import unittest
+from fearquantlib.wavelib import __max_successive_series_len as max_len
+
+
+class TestMxSuccSeriesLen(unittest.TestCase):
+
+    def test_fn(self):
+        arr = [1,7,3,4,5,2,4,5,6,1,0,4]
+        l = max_len(arr)
+        self.assertEqual(4,l)
+
+
+if __name__ == '__main__':
+    unittest.main()
